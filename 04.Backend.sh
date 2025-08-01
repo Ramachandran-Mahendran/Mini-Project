25c1722ff55a2bdc9824377abc8f2edc4b62f77a
Ramachandran-Mahendran <rcmaws1987@gmail.com>
2025-08-01 11:54:34 +0530

    Commit2

--- 04.Backend.sh
+++ 04.Backend.sh
@@ -77,7 +77,7 @@ VALIDATE $? "Enabling backend"
 dnf install mysql -y &>>$LOGFILE
 VALIDATE $? "Installing MySQL Client"
 
-mysql -h 172.31.85.88 -uroot -p${mysql_root_password} < /app/schema/backend.sql &>>$LOGFILE
+mysql -h 172.31.83.30 -uroot -p${mysql_root_password} < /app/schema/backend.sql &>>$LOGFILE
 VALIDATE $? "Schema loading"
 
 systemctl restart backend &>>$LOGFILE
